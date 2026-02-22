efa00a9bdbc44773b6c49c7127e7b54a5092870e
joannemdaniels5-nat <joannemdaniels5@gmail.com>
2026-02-22 03:50:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from datetime import date
 import io
 
-from rota_engine_v37_7 import read_template, build_workbook, ensure_monday, recalc_workbook_from_site_timelines
+from rota_engine_v37_8 import read_template, build_workbook, ensure_monday, recalc_workbook_from_site_timelines
 
 st.set_page_config(page_title="ModLew PSA Rota Generator", page_icon="🗓️", layout="wide")
 st.title("ModLew PSA Rota Generator")
